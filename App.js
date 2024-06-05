cfeadf31867adad256af9c3c48f2f203cacb0999
Joel Middleton <jl422083@dal.ca>
2024-06-05 17:40:11 -0300

    Message editing added.

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import { useEffect, useLayoutEffect } from 'react';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { TouchableOpacity, View, Text, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import MasonryList from '@react-native-seoul/masonry-list'
-import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation } from './db';
+import { useSearchNotesQuery, useAddNoteMutation, useDeleteNoteMutation, useUpdateNoteMutation } from './db';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 function HomeScreen({ navigation }) {
   const { data: searchData, error, isLoading } = useSearchNotesQuery("");
   const [ addNote, { data: addNoteData, error: addNoteError }] = useAddNoteMutation();
+  const [ editNote, {data: editNoteData, error: editNoteError}] = useUpdateNoteMutation();
   const [ deleteNote ] = useDeleteNoteMutation();
   
   useEffect(() => {
@@ -20,9 +22,19 @@ function HomeScreen({ navigation }) {
     }
   }, [addNoteData]);
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => deleteNote(item) } style={tw`w-[98%] mb-0.5 mx-auto bg-purple-300 rounded-sm px-1`}> 
-      <Text>{item.title} {item.id}</Text>
+  // May be able to combine this with the useEffect above, keeping seperate while testing
+  useEffect(() => {
+    if (editNoteData != undefined) {
+      console.log(editNoteData.title);
+      navigation.navigate("Edit", {data: editNoteData});
+    }
+  }, [editNoteData]);
+
+  // This is the preview of the note, right now it's a placeholder instead of a preview of the text
+  const renderItem = ({ item }) => 
+    (
+    <TouchableOpacity onPress={() => editNote(item) } style={tw`w-[98%] mb-0.5 mx-auto bg-purple-300 rounded-sm px-1`}> 
+      <Text>{item.id} {item.content}</Text> 
     </TouchableOpacity>
   )
 
@@ -47,12 +59,34 @@ function HomeScreen({ navigation }) {
 }
 
 function EditScreen({ route, navigation }) {
+  const [text, setText] = useState(route.params.data.content);
+  const [ updateNote, {data: updateNoteData}] = useUpdateNoteMutation();
+  
+  
   useLayoutEffect(() => {
     navigation.setOptions({ title: route.params.data.title });
   }, []);
 
-  return (
+  const onChangeText = (newText) => {
+    setText(newText);
+
+  };
+
+  useEffect(() => {
+    return () => {
+      updateNote({ id: route.params.data.id, content: text });
+    };
+  }, [text]);
+
+  return ( // Place title and id of the note in the center of the new note
     <View style={tw`flex-1 items-center justify-center bg-purple-400`}>
+      {<TextInput
+        style={tw`w-[98%] h-12 bg-white rounded-sm px-1`} 
+        onChangeText={onChangeText}
+        placeholder='Enter note here...'
+        value={text}
+      />}
+      <FontAwesomeIcon icon="fa-solid fa-trash-can" />
       <Text style={tw`text-lg text-white`}>Edit Screen {route.params.data.title} {route.params.data.id}</Text>
     </View>
   );
